9a79bd3aaf9dc58b2c62cd4268e896f62864117f
Abiskar <ghimirra@gmail.com>
2026-08-08 17:54:01 +0530

    updated cors

--- src/main/java/com/example/rag_backend/config/AppConfig.java
+++ src/main/java/com/example/rag_backend/config/AppConfig.java
@@ -21,7 +21,7 @@ public class AppConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173", "http://localhost:3000") // Frontend URLs
+                        .allowedOrigins("*") // Frontend URLs
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
