367edbeda00a46690cab7112fe8b7329de506636
Abiskar <ghimirra@gmail.com>
2026-09-26 20:50:42 +0530

    updated authentication

--- src/main/java/com/example/rag_backend/config/SecurityConfig.java
+++ src/main/java/com/example/rag_backend/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.example.rag_backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -32,5 +34,9 @@ public class SecurityConfig {
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
